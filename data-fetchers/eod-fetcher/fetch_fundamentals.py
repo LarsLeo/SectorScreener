469ae6d051247667a84a9fd4c4f18738f40d30fb
Lars Leo Grätz <larsleo.graetz@eon.com>
2025-08-21 16:58:11 +0200

    Revert "test: test gemini commit"
    This reverts commit b7a4b0a93b41ad3c4d847787d65dc89e77efccfc.

--- data-fetchers/eod-fetcher/fetch_fundamentals.py
+++ data-fetchers/eod-fetcher/fetch_fundamentals.py
@@ -52,7 +52,6 @@ def save_data_to_file(data, filepath):
         
     except Exception as e:
         print(f"Error saving data to file: {e}")
-        print(f"Error saving data to file: {e}")
         return None
 
 
